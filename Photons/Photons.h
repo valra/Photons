f2dd1e97f9892b26598e6aa5c9fa4f5105bd853d
Siddarth Kalra <sid@sidkalra.com>
2019-02-03 20:57:20 -0500

    feat: create project.

---
+++ Photons/Photons.h
@@ -0,0 +1,19 @@
+//
+//  Photons.h
+//  Photons
+//
+//  Created by Siddarth Kalra on 2019-02-03.
+//  Copyright © 2019 Valra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Photons.
+FOUNDATION_EXPORT double PhotonsVersionNumber;
+
+//! Project version string for Photons.
+FOUNDATION_EXPORT const unsigned char PhotonsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Photons/PublicHeader.h>
+
+
